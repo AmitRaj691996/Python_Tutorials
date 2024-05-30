4a46b52e5ac552e64bf97059f9c5dbad9ae75f2e
Amit Raj <amitraj691996@gmail.com>
2024-05-30 20:19:49 +0530

    Corrected the forward indexing   while traversing on DEV branch

--- List/C2.py
+++ List/C2.py
@@ -1,7 +1,7 @@
 l=[3,5,9,8,3,4,5,9,6,3,7,2]
 l.sort()
-for i in range(0,len(l)):
-    if (l.count(l[i]))>1:
-        l.pop(i)
+for x in l:
+    if l.count(x)>1:
+        l.pop(l.index(x))
 print(l)
 
