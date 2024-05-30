138efb5fcd0e2d8aaf288cd25233f9c7ee61c03d
Amit Raj <amitraj691996@gmail.com>
2024-05-30 22:51:49 +0530

    challenge C4 completed

---
+++ List/C4.py
@@ -0,0 +1,12 @@
+fav1=['pizza','nuggets','hotdog','noodles','pasta','burger']
+fav2=['burger','hotdog','noodles','pasta','nuggets','pizza']
+f=[]
+n=[]
+for i in range(len(fav1)):
+    temp=fav1[i]
+    j = fav2.index(temp)
+    f.append(temp)
+    n.append(i+j)
+print(f,'\n',n)
+indx=n.index(min(n))
+print('Please have',f[indx],'as your favourite dish')
