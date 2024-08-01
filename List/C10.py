85c53669764494b05c30978578aed8de5898e5d0
Amit Raj <amitraj691996@gmail.com>
2024-08-01 20:07:44 +0530

    Added comment in C10.py

--- List/C10.py
+++ List/C10.py
@@ -1,5 +1,5 @@
 food = ['pizza', 'nuggets', 'hotdog', 'noodles', 'pasta', 'burger']
-print(food[0][0])
+#print(food[0][0]) To access first letter of food item in list food
 letter=input('Please, Enter the letter!!\n')
 for i in range(0,len(food)):
     if food[i].startswith(letter.upper()) or food[i].startswith(letter.lower()) :
